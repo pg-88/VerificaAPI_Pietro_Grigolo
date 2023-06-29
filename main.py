e64007c412e20cfb51d04443ec3a590f0152b82d
Pietro Grigolo <grigolopi@gmail.com>
2023-06-29 11:17:26 +0200

    API con fastAPI

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Depends, status
+from fastapi import FastAPI, Depends
+from fastapi.logger import logger as fastapi_logger
 import uvicorn
 from pydantic import BaseModel
 import joblib
@@ -7,48 +8,55 @@ import joblib
 
 # struttura dati che ricalca gli input necessari al modello
 class ModelInput(BaseModel):
-    tv: float = 147.042 # default il vslore medio
-    radio: float = 23.26 # default il vslore medio
-    newspaper: float = 30.55 # default il vslore medio
+    rd: float = 73721.61 # default il vslore medio
+    admin: float = 121344.64 # default il vslore medio
+    market: float = 211025.10 # default il vslore medio
 
 ##########################################################################
 
-app = FastAPI(title='Sales API', description='genera previsioni di future vendite sfruttando il modello. Autore Pietro Griolo')
+app = FastAPI(title='Profit API', 
+              description='''
+              genera previsioni di futuri profitti sfruttando il modello. 
+              \n
+              Autore Pietro Griolo'''
+              )
 
 # caricare il modello come var globale all'avvio del servizio
 @app.on_event("startup")
 def on_startup():
     global model
-    with open('sales.pkl', 'rb') as pickle:
-        model = joblib.load(pickle)
-        print('Caricsto il modello')
-
+    try:
+        with open('profit.pkl', 'rb') as pickle:
+            model = joblib.load(pickle)
+            fastapi_logger.log(level=50, msg='Caricato il modello')
+    except:
+        fastapi_logger.log(50, "Problema nel caricamento del modello", exc_info=1)
     return model
-
 
 @app.get("/")
 def hello():
     return {"<----     http://localhost:8000/docs     ------>"}
 
 # chiamata GET
-@app.get("/sales")
+@app.get("/profit")
 async def get_sales(in_data: ModelInput=Depends()):
-    X = [[in_data.tv, in_data.radio, in_data.newspaper]]
-    sales = model.predict(X)[0]
-    print(sales)
-    
-    return {"Sales": sales}
+    try:
+        X = [[in_data.rd, in_data.admin, in_data.market]]
+        res = round(model.predict(X)[0], 2)
+        return {"Profit": res}
+    except:
+        return {"Result": "Errore"} 
 
 # chiamata POST
-@app.post("/sales")
+@app.post("/profit")
 async def post_sales(in_data: ModelInput):
-    pass
+    try:
+        X = [[in_data.rd, in_data.admin, in_data.market]]
+        res = round(model.predict(X)[0], 2)
+        return {"Profit": res}
+    except:
+        return {"Result": "Errore"} 
+
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
-
-'''
-################# to do #####################
-Usare dotenv per indirizzi (base URL) e nomi files
-
-'''
